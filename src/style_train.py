38e65f7918b88240d8864afd00322d14684b7e34
kenjune <zhuyj40@126.com>
2025-04-15 19:26:22 +0800

    fix some bugs

--- src/style_train.py
+++ src/style_train.py
@@ -33,6 +33,9 @@ def evaluate_model(model, loader, criterion, device):
     model.eval()
     y_true=[]
     y_pred=[]
+    total_loss = 0.0
+    correct = 0
+    total =0
     with torch.no_grad():
         for inputs, labels in loader:
             inputs, labels = inputs.to(device), labels.to(device)
